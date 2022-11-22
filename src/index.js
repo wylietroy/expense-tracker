cb099c20a2176de8ad18ff64cd9093d38032d989
Jim Clark <jim.clark@generalassemb.ly>
2022-11-22 14:01:51 -0800

    sync-2-part-2-finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './pages/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router><App /></Router>
   </React.StrictMode>
 );
 
