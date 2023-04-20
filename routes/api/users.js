f5d994190df20cd6fceadd7dc8fea84c7fda2ed6
BoognishSaves <johnpaulhaddad@hotmail.com>
2023-04-20 11:08:21 -0600

    MERN Infrastructure complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
+// require the authorization middleware function
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 // GET /api/users/check-token
-router.get('/check-token', usersCtrl.checkToken);
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 
 // POST /api/users
 router.post('/', usersCtrl.create);
