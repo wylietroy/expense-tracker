324a864fb32f9f6e7fb2e3b6bea3fdd1d16999a5
wylietroy <wyliesebtroy@gmail.com>
2023-04-30 20:03:35 -0400

    nav bar as well

--- src/ExpenseList/ExpenseList.jsx
+++ src/ExpenseList/ExpenseList.jsx
@@ -1,28 +1,5 @@
 import React from 'react';
 
-// function ExpenseList(props) {
-//     return (
-//         <table className="table">
-//             <thead>
-//                 <tr>
-//                     <th>Title</th>
-//                     <th>Amount</th>
-//                 </tr>
-//             </thead>
-//             <tbody>
-//                 {props.expense.map((expense, index) => (
-//                     <tr key={index}>
-//                         <td>{expense.title}</td>
-//                         <td>{expense.amount}</td>
-//                     </tr>
-//                 ))}
-                
-
-//             </tbody>
-//         </table>
-//     );
-// }
-
 function ExpenseList(props) {
     return (
       <div>
